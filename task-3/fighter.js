ceb68f312287015727a2e4e555f9982a6597d9bc
will <will@pop-os.localdomain>
2022-02-17 12:25:57 +0800

    task 3 added

---
+++ task-3/fighter.js
@@ -0,0 +1,30 @@
+// constructor function
+function Fighter() {
+  this.name = "";
+  this.strength = 20;
+  this.agility = 23;
+  this.vitality = 11;
+
+  this.getName = function () {
+    return this.name;
+  };
+
+  this.getHp = function () {
+    return this.name;
+  };
+
+  this.takeDamage = function () {
+    return this.name;
+  };
+
+  this.dealDamage = function (damage) {
+    return this.name;
+  };
+}
+
+function fight(figher1, figher2) {}
+
+let fighter1 = new Fighter();
+let fighter2 = new Fighter();
+
+fight(fighter1, fighter2);
